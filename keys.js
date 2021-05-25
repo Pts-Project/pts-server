77d72c0a8d698d2e6ef966bc32a3538819807dd1
Tanmya Vishvakarma <tanmya2000@gmail.com>
2021-05-25 12:15:44 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://Platform:o3EumnIMceQfPiwQ@cluster0.lsibt.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    MONGOURI:"mongodb+srv://ADMIN:hPkyJ2eJbGdDboxx@cluster0.kwzfd.mongodb.net/portal?retryWrites=true&w=majority",
     JWTSECRET:"zvzvzvzvzvz",
     SENDGRID_API: "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",
     EMAIL: "http://localhost:3000"
